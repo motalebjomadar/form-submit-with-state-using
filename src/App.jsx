53c8529fd2320d87d65bd350c0bd1d52ecd5eb9a
motalebjomadar <motalabjomadar@gmail.com>
2024-07-07 10:59:25 +0800

    form submit with state using and use also on change

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import StateFullForm from './components/StateFullForm'
+
+function App() {
+
+  return (
+    <>
+      
+      <h2>Vite + React</h2>
+      <StateFullForm></StateFullForm>
+
+      
+    </>
+  )
+}
+
+export default App
